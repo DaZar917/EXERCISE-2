6f700f425ed2b25c319bdb402d0079190e9b6971
Yaroslav Melnychyn <yaroslav.melnychyn@student.tuke.sk>
2020-12-31 11:50:43 +0100

    final commit

--- src/com/company/BruteForceAlgorithm.java
+++ src/com/company/BruteForceAlgorithm.java
@@ -17,24 +17,24 @@ public class BruteForceAlgorithm {
     public void count_ways(){
         List<Integer> usable_values = separate_usable_values(this.sum, this.unusable_values);
         int size = usable_values.size();
-        initialization_brute(usable_values, size, sum);
+        initialization_brute(usable_values, size, this.sum);
         System.out.println("The algorithm find " + this.ways + " different ways.");
     }
 
-    private void initialization_brute(List<Integer> arr, int size, int brute_size) {
+    private void initialization_brute(List<Integer> numbers, int size, int brute_size) {
         List<Integer> chosen = new ArrayList<>();
-        recursive_brute(chosen, arr, 0, brute_size, 0, size - 1);
+        recursive_brute(chosen, numbers, 0, brute_size, 0, size - 1);
     }
 
-    private void recursive_brute(List<Integer> chosen, List<Integer> arr, int index, int brute_size, int start, int end) {
+    private void recursive_brute(List<Integer> chosen, List<Integer> numbers, int index, int brute_size, int start, int end) {
         int sum_of_line = 0;
         if (index == brute_size) {
             for (int i = 0; i < brute_size; i++) {
-                if (arr.get(chosen.get(i)) != 0)
-                    System.out.printf("%d ", arr.get(chosen.get(i)));
-                sum_of_line = sum_of_line + arr.get(chosen.get(i));
+                if (numbers.get(chosen.get(i)) != 0)
+                    System.out.printf("%d ", numbers.get(chosen.get(i)));
+                sum_of_line = sum_of_line + numbers.get(chosen.get(i));
             }
-            if (sum_of_line == sum){
+            if (sum_of_line == this.sum){
                 System.out.print(" - the new way");
                 ways++;
             }
@@ -43,7 +43,7 @@ public class BruteForceAlgorithm {
         }
         for (int i = start; i <= end; i++) {
             chosen.add(index,i);
-            recursive_brute(chosen, arr, index + 1, brute_size, i, end);
+            recursive_brute(chosen, numbers, index + 1, brute_size, i, end);
         }
     }
 }
